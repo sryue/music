a025b94d2dd976ce0a76209b253dcae9a43a02ab
blangeblange <1102160902@qq.com>
2017-02-13 15:11:30 +0800

    提交用户模块

--- frontend/controllers/UsersController.php
+++ frontend/controllers/UsersController.php
@@ -3,6 +3,8 @@ namespace frontend\controllers;
 
 use Yii;
 use yii\web\Controller;
+use yii\db\Query;
+use yii\data\Pagination;
 
 /**
  * 用户页面
@@ -21,7 +23,40 @@ class UsersController extends Controller
 
 	//用户展示页面
 	public function actionShow(){
-		return $this->render('show');
+		header("content-type:text/html;charset=utf-8");
+
+		//实例化query
+		$query=new Query();
+
+		$query->from('ais_user');
+
+		//分页
+		$pagination = new Pagination(['totalCount' => $query->count()]);
+
+		//条数
+		$pagination->setPageSize('5');
+
+		//条件
+		$query->offset($pagination->offset)->limit($pagination->limit);
+
+		//执行
+		$userInfo=$query->all();
+
+		//print_r($userInfo);die;
+
+		return $this->render('show',['data'=>$userInfo,'page'=>$pagination]);
+	}
+	public function actionDel(){
+		$request=\YII::$app->request;
+		$id=$request->post('ids');
+		$connection = \Yii::$app->db;
+		$sql="delete from ais_user where user_id='$id'";
+		$re = $connection->createCommand($sql)->execute();
+		if($re){
+			echo 1;
+		}else{
+			echo 0;
+		}
 	}
 	
 }
